240c1ff11eb3e4d9b2992d7c2183adfb104f2a1e
Alina Turdakunova <turdakunova@Alinas-MacBook-Pro.local>
2023-12-08 23:38:59 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import { useEffect, useState } from "react";
+import { TodoItem } from "./Components/Todoitem";
+import './App.css'
+
+ function App() {
+  const [todos,setTodos] = useState([])
+  const [inputValue,setInputValue ] = useState('')
+
+
+const createTodo=()=>{
+  setTodos([...todos,{text:inputValue,complete:true}])
+  setInputValue("")
+}
+
+
+  return (
+    <div className="App">
+     <div>
+      <input type="text"
+      value={inputValue}
+      onChange={(e)=>{
+        setInputValue(e.target.value)
+      }}/>
+      <button onClick={createTodo}>ADD</button>
+     </div>
+         <div>
+          {
+            todos.map((el,id)=>{
+              return <TodoItem todo={el} key={id}/>
+            })
+          }
+         </div>
+    </div>
+  )
+}
+export default App
---
+++ src/Components/Todoitem/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const  TodoItem= ({todo}) => {
+  return (
+    <div className='todo-item'>{todo.text}
+    <button>complete</button>
+    <button>delete</button>
+    </div>
+    
+  )
+}
+
+
